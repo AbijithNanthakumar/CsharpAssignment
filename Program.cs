cf83b01cc7dedb0d70bb5fe79554ffea4a5055bf
AbijithNanthakumar <abijith.nanthakumar@psiog.com>
2025-09-30 22:07:47 +0530

    const changes made

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿
+using System;
+
+// Constructor chaining and Method Overloading
+//public class Student
+//{
+//    string name;
+//    int age;
+//    int marks;
+
+//    public Student()
+//    {
+//        this.name = name;
+//        this.age = age;
+//    }
+
+//    public Student(string name,int age) : this() 
+//    {
+//        this.name = name;
+//        this.age = age;
+//    }
+
+//    public Student(string name,int age,int marks) : this(name,age)
+//    {
+//        this.marks = marks;
+
+
+//    }
+
+//    public void display()
+//    {
+//        Console.WriteLine(name+"  "+age+"  "+marks);
+//    }
+//}
+
+
+//class Program
+//{
+//    static void Main()
+//    {
+//       Student student = new Student("Abijith",22,95);
+//       student.display();
+//    }
+//}
+
+
+// Const and ReadOnly
+
+class Student
+{
+    public const int ab = 10;
+
+    //ab = 20; // Cannot be modified
+}
+//class Program
+//{
+    
+//    static void Main()
+//    {
+        
+//    }
+//}
